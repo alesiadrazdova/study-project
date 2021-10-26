eee2e37d31c1572c3f06c07ecb39d91bab524252
Alesia Drazdova <alesiadrozdova@gmail.com>
2021-10-26 19:47:11 +0300

    created footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from '../../logo.svg';
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <div className='footer-wrapper'>
+            <div className='footer'>
+                <div className='logo-footer'>
+                    <img src={logo} className="app-logo" alt="logo" />
+                </div>
+                <div className='copyright'>
+                    <p>© Copyright Event 2021. All Right Reserved.</p>
+                </div>
+            </div>
+        </div>
+    )
+};
