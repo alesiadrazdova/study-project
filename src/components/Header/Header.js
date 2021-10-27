419cab48ff2148e419175ca46b6fc2eeca926dff
Alesia Drazdova <alesiadrozdova@gmail.com>
2021-10-27 18:38:19 +0300

    creating links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import { AppBar, Box, Toolbar, Button } from '@mui/material';
 import logo from '../../logo.svg';
 
@@ -8,15 +9,18 @@ const Header = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
-                <Toolbar>
+                <Toolbar className='wrapper-header'>
                     <div className='logo-wrapper'>
                         <img src={logo} className="app-logo" alt="logo" />
                     </div>
+                    <div className='links-header'>
+                        <a href='/'>Find Events</a>
+                        <a href='/'>My Tickets</a>
+                        <a href='/'>Help</a>
+                    </div>
                     <div className='button-wrapper'>
                         <Button color='inherit' className='btn-header'>Login</Button>
-                        <Button variant='outlined' color='inherit' className='btn-header'>Signup</Button>
                     </div>
-
                 </Toolbar>
             </AppBar>
         </Box>
